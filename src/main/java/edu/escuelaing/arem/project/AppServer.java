c673de754e8bcbd5315e75db2c76b2f566187f86
jcamilovelandiab <jcamilovelandiab@gmail.com>
2019-09-01 17:33:41 -0500

    tratando de desplegar en heroku

--- src/main/java/edu/escuelaing/arem/project/AppServer.java
+++ src/main/java/edu/escuelaing/arem/project/AppServer.java
@@ -8,6 +8,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Method;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -16,6 +17,7 @@ import java.util.HashMap;
 import javax.imageio.ImageIO;
 
 import edu.escuelaing.arem.project.notation.Web;
+
 
 public class AppServer {
 
@@ -36,7 +38,6 @@ public class AppServer {
 		for(Method m : c.getMethods()){
             if(m.isAnnotationPresent(Web.class)){
             	Handler handler = new UrlHandler(m);
-            	System.out.println(m.getAnnotation(Web.class).url());
                 listUrl.put("/apps/"+m.getAnnotation(Web.class).url(), handler);
             }
         }
@@ -44,10 +45,11 @@ public class AppServer {
 	
 	public static void listen() throws IOException {
 		ServerSocket serverSocket = null;
+		int port = getPort();
         try {
-            serverSocket = new ServerSocket(80);
+            serverSocket = new ServerSocket(port);
         } catch (IOException e) {
-            System.err.println("Could not listen on port: 80.");
+            System.err.println("Could not listen on port: "+port+".");
             System.exit(1);
         }
         while(true){
@@ -65,56 +67,13 @@ public class AppServer {
             String inputLine;
             
             while ((inputLine = in.readLine()) != null) {
-                int begin,end;
-                String urlInputLine;
                 if(inputLine.contains("/apps/")) {
-                	begin = inputLine.indexOf("/apps/");
-                	end = inputLine.indexOf("HTTP")-1;
-                	
-                	String resource = inputLine.substring(begin, end);
-                	out.println("HTTP/2.0 200 OK");
-                    out.println("Content-Type: text/html");
-                    out.println("\r\n");
-                    //System.err.println("What: " + resource);
-                    out.println(listUrl.get(resource).process());
-                	
-                }else if(inputLine.contains("resources")) {
+                	sendAPP(inputLine, out);
+                }else if(inputLine.contains("/resources/")) {
 	                if (inputLine.contains(".html")) {
-	                	end = inputLine.indexOf(".html");
-	                	begin = end-1;
-	                	while(begin>=0 && inputLine.charAt(begin)!='/') {
-	                		begin--;
-	                	}
-	                	urlInputLine = inputLine.substring(begin+1,end+5);
-	                    String urlDirectoryServer = System.getProperty("user.dir") + "\\resources\\html\\" + urlInputLine;
-	                    try {
-	                        BufferedReader readerFile = new BufferedReader(new InputStreamReader(new FileInputStream(urlDirectoryServer), "UTF8"));
-	                        out.println("HTTP/2.0 200 OK");
-	                        out.println("Content-Type: text/html");
-	                        out.println("\r\n");
-	                        while (readerFile.ready()) {
-	                            out.println(readerFile.readLine());
-	                        }
-	                    }catch (FileNotFoundException e) {
-	                        //out.println("HTTP/2.0 404 Not found.");
-	                        //out.println("Content-Type: text/html");
-	                        //out.println("\r\n");
-	                    }
+	                	sendHTML(inputLine, out);
 	                }else if(inputLine.contains(".jpg")){
-	                	end = inputLine.indexOf(".jpg");
-	                	begin = end-1;
-	                	while(begin>=0 && inputLine.charAt(begin--)!='/');
-	                	urlInputLine = inputLine.substring(begin+1,end+4);
-	                	try {
-	                		String urlDirectoryServer = System.getProperty("user.dir") + "\\resources\\jpg\\" +urlInputLine;
-	                        BufferedImage img = ImageIO.read(new File(urlDirectoryServer));
-	                        out.println("HTTP/2.0 200 OK");
-	                        out.write("Content-Type: image/webp,*/*");
-	                        out.println("\r\n");
-	                        ImageIO.write(img, "jpg",clientSocket.getOutputStream());
-	                	}catch(Exception e) {
-	                		//System.err.println("I CANT READ "+inputLine);
-	                	}
+	                	sendJPG(inputLine, clientSocket, out);
 	                }
                 }
                 if (!in.ready()) {
@@ -128,4 +87,64 @@ public class AppServer {
         //serverSocket.close();
     }
 	
+	public static void sendAPP(String inputLine, PrintWriter out) {
+		int begin = inputLine.indexOf("/apps/");
+    	int end = inputLine.indexOf("HTTP")-1;
+    	
+    	String resource = inputLine.substring(begin, end);
+    	out.println("HTTP/2.0 200 OK");
+        out.println("Content-Type: text/html");
+        out.println("\r\n");
+        //System.err.println("What: " + resource);
+        out.println(listUrl.get(resource).process());
+	}
+	
+	public static void sendHTML(String inputLine, PrintWriter out) {
+		int end = inputLine.indexOf(".html");
+    	int begin = end-1;
+    	while(begin>=0 && inputLine.charAt(begin)!='/') {
+    		begin--;
+    	}
+    	String urlInputLine = inputLine.substring(begin+1,end+5);
+        String urlDirectoryServer = System.getProperty("user.dir") + "\\resources\\html\\" + urlInputLine;
+        try {
+            BufferedReader readerFile = new BufferedReader(new InputStreamReader(new FileInputStream(urlDirectoryServer), "UTF8"));
+            
+            out.println("HTTP/2.0 200 OK");
+            out.println("Content-Type: text/html");
+            out.println("\r\n");
+            while (readerFile.ready()) {
+                out.println(readerFile.readLine());
+            }
+        }catch (Exception e) {
+            //out.println("HTTP/2.0 404 Not found.");
+            //out.println("Content-Type: text/html");
+            //out.println("\r\n");
+        }
+	}
+	
+	public static void sendJPG(String inputLine, Socket clientSocket, PrintWriter out) {
+		int end = inputLine.indexOf(".jpg");
+    	int begin = end-1;
+    	while(begin>=0 && inputLine.charAt(begin--)!='/');
+    	String urlInputLine = inputLine.substring(begin+1,end+4);
+    	try {
+    		String urlDirectoryServer = System.getProperty("user.dir") + "\\resources\\jpg\\" +urlInputLine;
+            BufferedImage img = ImageIO.read(new File(urlDirectoryServer));
+            out.println("HTTP/2.0 200 OK");
+            out.write("Content-Type: image/webp,*/*");
+            out.println("\r\n");
+            ImageIO.write(img, "jpg",clientSocket.getOutputStream());
+    	}catch(Exception e) {
+    		//System.err.println("I CANT READ "+inputLine);
+    	}
+	}
+	
+	public static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 35000; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 }
